ff68e22b99ea666a3d6f90d1581f02743f0515b3
Vassterak <vaclavhorcicka@outlook.cz>
2022-01-01 23:23:46 +0100

    Clean up

---
+++ Database.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace pokladnaInitial
+{
+    public static class DatabaseConnection
+    {
+        private static void DatabzeTest()
+        {
+
+            // případné vytvoření databáze (jen poprvé)
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            Console.WriteLine("Vytvořena databáze.");
+
+            // připojení k databázi
+            SQLiteConnection m_dbConnection;
+            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+            string sql;
+            SQLiteCommand command;
+
+            // případné vytvoření tabulky (jen jednou)
+            sql = "CREATE TABLE highscores (jmeno VARCHAR(20), skore INT)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            Console.WriteLine("Vytvořena tabulka.");
+
+            // naplnění tabulky daty
+            sql = "insert into highscores (jmeno, skore) values ('Petříček', 3000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (jmeno, skore) values ('Janička', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "insert into highscores (jmeno, skore) values ('Já Sám ', 9009)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            Console.WriteLine("Vložena data (Petříček-3000, Janička-6000, Já Sám-9009).");
+
+
+            // výpis dat z tabulky
+            Console.WriteLine("Výpis podle bodů:");
+            Console.WriteLine("Jméno:   \t Skóre:");
+            sql = "select * from highscores order by skore desc";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine(reader["jmeno"] + " \t " + reader["skore"]);
+
+            // uzavření připojení k databázi
+            m_dbConnection.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Data.SQLite;
 using System.Text.RegularExpressions;
 
 namespace pokladnaInitial
@@ -23,61 +22,6 @@ namespace pokladnaInitial
     public partial class MainWindow : Window
     {
         bool readNewBarcode = false;
-        List<Product> items = new List<Product>();
-        public MainWindow()
-        {
-
-        }
-
-        private void DatabzeTest()
-        {
-            InitializeComponent();
-
-            // případné vytvoření databáze (jen poprvé)
-            SQLiteConnection.CreateFile("MyDatabase.sqlite");
-            Console.WriteLine("Vytvořena databáze.");
-
-            // připojení k databázi
-            SQLiteConnection m_dbConnection;
-            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
-            m_dbConnection.Open();
-            string sql;
-            SQLiteCommand command;
-
-            // případné vytvoření tabulky (jen jednou)
-            sql = "CREATE TABLE highscores (jmeno VARCHAR(20), skore INT)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            Console.WriteLine("Vytvořena tabulka.");
-
-            // naplnění tabulky daty
-            sql = "insert into highscores (jmeno, skore) values ('Petříček', 3000)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            sql = "insert into highscores (jmeno, skore) values ('Janička', 6000)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            sql = "insert into highscores (jmeno, skore) values ('Já Sám ', 9009)";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            command.ExecuteNonQuery();
-            Console.WriteLine("Vložena data (Petříček-3000, Janička-6000, Já Sám-9009).");
-
-
-            // výpis dat z tabulky
-            Console.WriteLine("Výpis podle bodů:");
-            Console.WriteLine("Jméno:   \t Skóre:");
-            sql = "select * from highscores order by skore desc";
-            command = new SQLiteCommand(sql, m_dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-                Console.WriteLine(reader["jmeno"] + " \t " + reader["skore"]);
-
-            // uzavření připojení k databázi
-            m_dbConnection.Close();
-
-            Console.ReadLine();
-        }
-
 
         private void ScreenKeyDown(object sender, KeyEventArgs e)
         {
@@ -142,15 +86,6 @@ namespace pokladnaInitial
             gView.Columns[2].Width = workingWidth * col3;
             gView.Columns[3].Width = workingWidth * col4;
         }
-
-        private int GetASCII(KeyEventArgs e)
-        {
-            char parsedCharacter = ' ';
-            if (Char.TryParse(e.Key.ToString(), out parsedCharacter))
-                textCleared.Content = (int)parsedCharacter;
-
-            return 0;
-        }
     }
 
     public class Product
@@ -161,5 +96,3 @@ namespace pokladnaInitial
         public int Count { get; set; }
     }
 }
-
-//https://wpf-tutorial.com/listview-control/listview-with-gridview/
